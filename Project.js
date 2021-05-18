c2eb42685615c7c6f425854c5d31c7f685fa6e49
thomasthengumpally <75409291+thomasthengumpally@users.noreply.github.com>
2021-05-18 20:29:07 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,12 @@
+function purple__btn()
+{
+    document.body.style.background="purple"
+}
+function green__btn()
+{
+    document.body.style.background="green"
+}
+function yellow__btn()
+{
+    document.body.style.background="yellow"
+}
